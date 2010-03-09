b1d1a7a74054dafff577449b8a10526048fd77a5
Cyril David <cyx.ucron@gmail.com>
2010-03-09 21:37:40 +0800

    removed vendored stuff, converting to pure RVM no more dependencies

--- vendor/carrierwave-0.4.5/features/support/datamapper.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-require 'dm-core'
-require 'carrierwave/mount'
-require 'carrierwave/orm/datamapper'
-
-DataMapper.setup(:default, 'sqlite3::memory:')
--- vendor/sinatra-0.9.5/lib/sinatra.rb
+++
@@ -1,8 +0,0 @@
-libdir = File.dirname(__FILE__)
-$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-
-require 'sinatra/base'
-require 'sinatra/main'
-require 'sinatra/compat'
-
-enable :inline_templates
